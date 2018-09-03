8f40527dca86b340fabd5320e7b3f4362f375eca
KRDodge <20012011ryu@gmail.com>
2018-09-03 10:55:07 +0900

    sdfgsdfg

---
+++ ConsoleApplication1/ConsoleApplication1/CoutFloat.cpp
@@ -0,0 +1,15 @@
+#include"stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int cout_float()
+{
+	float f = 65.5f;
+	cout << "float = " << f << endl;
+	cout << "int = "<<(int)f <<endl;
+	cout << "char = "<< (char)f<<endl;
+
+	return 0;
+}
---
+++ ConsoleApplication1/ConsoleApplication1/HelloAscii.cpp
@@ -0,0 +1,13 @@
+#include"stdafx.h"
+#include<iostream>
+
+using namespace std;
+
+int hello_ascii()
+{
+	int a[11]={72,69,76,76,79,32,87,79,82,76,68};
+
+
+	cout << (char)a<< endl;;
+	return 0;
+}
